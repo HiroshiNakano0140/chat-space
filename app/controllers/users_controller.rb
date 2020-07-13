249aeb5d69afd08f9135bdfceb28ba362479dfde
HiroshiNakano0140 <changum0140@gmail.com>
2020-07-13 19:00:21 +0900

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+    def index
+      return nil if params[:keyword] == ""
+      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    end    
   end
 
   private
